0c93a9293844ebdfef72d87db78043c61e86d7e3
bryant <bryantkrause@gmail.com>
2020-09-17 12:32:47 -0700

    I have created life!!!

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import {
+BrowserRouter as Router,
+Switch,
+Route
+} from 'react-router-dom'
 import Tasks from './pages/Tasks'
+import Page2 from './pages/page2'
 
 const App = () =>{
   return (
-    <div className="container">
-      <Tasks />
+    <div>
+      <Router>
+        <Switch>
+        <Route exact path="/">
+          <Tasks/>
+        </Route>
+
+        <Route path="/page2">
+          <Page2/>
+        </Route>
+
+        </Switch>
+      </Router>
     </div> 
   )
 }
---
+++ client/src/pages/page2/page2.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import axios from 'axios'
+import TaskContext from '../../utils/TaskContext'
+import TaskForm from '../../components/TaskForm'
+import TaskDisplay from '../../components/TaskDisplay'
+
+class Page2 extends React.Component {
+  state = {
+    task: '',
+    status: '',
+    tasks: [],
+    inputChange: e => {
+      this.setState({ [e.target.name]: e.target.value })
+    },
+    taskSubmit: e => {
+      e.preventDefault()
+      console.log(e)
+      //this.setState({ [e.target.name]: e.target.value })
+      axios.post(`/task`, {
+        task: this.state.task,
+        status: this.state.status
+     })
+        .then( ({data}) => {
+          let arr = JSON.parse(JSON.stringify(this.state.tasks))
+          arr.push(data)
+          this.setState({ tasks: arr, task: '', status: ''  })
+        })
+    },
+    deleteTask: _id => {
+      console.log(_id)
+      axios.delete(`/task/${_id}`,)
+      .then( r => {
+        axios.get('/tasks')
+        .then( ({data}) => this.setState({ tasks: data }))
+        console.log('data deleted')
+        console.log(this.state)
+      })
+      .catch(e => console.error('cant delete', e))
+    },
+    getTasks: () => {
+      console.log('getting tasks')
+      axios.get(`/tasks`)
+      .then (r =>{
+        console.log('tasks recieved')
+      })
+      .catch(e => console.error('no task for you', e))
+    }
+  }
+
+  componentDidMount() { // When the page loads grab the data from the database and update the tasks array
+    axios.get('/tasks')
+      .then( ({data}) => this.setState({ tasks: data }))
+  }
+
+  render () {
+    return (
+      <TaskContext.Provider value={this.state}>
+          <TaskDisplay />
+          <TaskForm />
+          </TaskContext.Provider>
+    )
+  }
+}
+
+export default Page2
